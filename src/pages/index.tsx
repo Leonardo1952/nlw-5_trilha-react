3c158c69ed00f447acc05e3cabdc7dc2e576d5c6
Leonardo <lo88980@gmail.com>
2021-04-21 11:14:05 -0300

    aula03 - formatacao dos dados

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,67 @@
-// SPA
-// SSR
-// SSG
+import { GetStaticProps } from 'next';
+import { format, parseISO} from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR'
+import { api } from '../services/api';
+import { convertDurationToTimeString } from '../utils/conertDurationToTimeString';
 
-import { useEffect } from "react"
+type Episode = {
+  id: string;
+  title: string;
+  thumbnail: string;
+  description: string;
+  members: string;
+  duration: number;
+  durationAssString: String;
+  url:string;
+  published_at: string;
+}
 
-export default function Home(props) {
-
+type HomeProps = {
+  episodes: Episode[]
     
 
+}
 
+export default function Home(props: HomeProps) {
+
+  
   return (
     <div>
-    <h1>Index</h1>
-    <p>{JSON.stringify(props.episodes)}</p>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
     </div>
   )
 }
 
 
-export async function getStaticProps() {
-  const response = await fetch('http://localhost:3333/episodes')
-  const data = await response.json()
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMMM yy', {locale: ptBR}),
+      duration: Number(episode.file.duration),
+      durationAssString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.file.url,
+    };
+
+  })
+
 
   return {
     props: {
-      episodes: data,
+      episodes,
     },
     revalidate: 60*60*8,
   }
